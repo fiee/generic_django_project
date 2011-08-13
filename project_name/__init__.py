9114811037905e07b1616af0c55d0267181627c4
Henning Hraban Ramm <hraban@fiee.net>
2011-08-13 11:21:49 +0200

    add version info (from djang patterns)

--- project_name/__init__.py
+++ project_name/__init__.py
@@ -5,4 +5,22 @@ from django.utils.translation import ugettext_lazy as _
 verbose_name = _(u'project_name')
 verbose_name_plural = _(u'project_names')
 
+# see http://djangopatterns.com/patterns/app_construction/version_reporting/
+__version_info__ = {
+    'major': 0,
+    'minor': 0,
+    'micro': 1,
+    'releaselevel': 'alpha',
+    'serial': 1
+}
 
+def get_version(short=False):
+    vers = ["%(major)i.%(minor)i" % __version_info__, ]
+    
+    if __version_info__['micro']:
+        vers.append(".%(micro)i" % __version_info__)
+    if __version_info__['releaselevel'] != 'final' and not short:
+        vers.append('%(releaselevel)s%(serial)i' % __version_info__)
+    return ''.join(vers)
+
+__version__ = get_version()
