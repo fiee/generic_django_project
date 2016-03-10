23f23bbeb89440ff824960568d2024af056dfd03
Henning Hraban Ramm <hraban@fiee.net>
2016-03-10 15:36:42 +0600

    revised server setup (including SSL settings)

--- server-setup/gunicorn-settings.py
+++ server-setup/gunicorn-settings.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-from project_name import settings
 
-bind = '127.0.0.1:8'+str(os.getuid())[1:]
-workers = 2
+PROJECT_NAME = 'project_name'
+rootrel = lambda p: os.path.normpath(os.path.join('/var/www', PROJECT_NAME, p))
+
+bind = 'unix:' + rootrel('run/django.socket')
+#bind = '127.0.0.1:8'+str(os.getuid())[1:]
+workers = 1
 #worker_class = 'eventlet'
 #max_requests = 2048
-pidfile = settings.rootrel('logs/django.pid')
-user = settings.PROJECT_NAME
-group = settings.PROJECT_NAME
-logfile = settings.rootrel('logs/gunicorn.log')
+pidfile = rootrel('run/django.pid')
+user = PROJECT_NAME
+group = PROJECT_NAME
+logfile = rootrel('logs/gunicorn.log')
 #loglevel = 'info'
-proc_name = 'gunicorn-'+settings.PROJECT_NAME
+proc_name = 'gunicorn-'+PROJECT_NAME
