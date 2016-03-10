5ef93813822b9c9bbffebf167af6124ab10b788d
Henning Hraban Ramm <hraban@fiee.net>
2016-03-10 15:35:30 +0600

    fix location of .env

--- django_project/manage.py
+++ django_project/manage.py
@@ -8,7 +8,7 @@ current_dir = os.path.dirname(__file__)
 # try to load .env file from current directory (I use that in development); 
 # if that fails (you’ll get a harmless warning), try the project’s base path.
 if not dotenv.load_dotenv(os.path.join(current_dir, '.env')):
-    dotenv.load_dotenv(os.path.abspath(os.path.join(current_dir, '../..', '.env'))) # '/var/www/project_name/.env'
+    dotenv.load_dotenv(os.path.abspath(os.path.join(current_dir, '../../..', '.env'))) # '/var/www/project_name/.env'
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
