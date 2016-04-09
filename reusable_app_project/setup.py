9df5941e31cb6a58785f05debe7b5ef8162048a3
Henning Hraban Ramm <hraban@fiee.net>
2016-04-09 17:20:09 +0600

    DARM: name changes to reusable_app_project

--- reusable_app_project/setup.py
+++ reusable_app_project/setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django_project',
+    name='reusable_app_project',
     version='0.1',
     packages=['django_project'],
     include_package_data=True,
@@ -23,7 +23,7 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License', # example license
+        'License :: OSI Approved :: BSD License',  # example license
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         # Replace these appropriately if you are stuck on Python 2.
