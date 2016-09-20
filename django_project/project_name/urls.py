4322d2e9c0947b9eefb148c3ef8751aeda982ab2
Henning Hraban Ramm <hraban@fiee.net>
2016-09-20 11:05:44 +0200

    Django 1.10 compat: drop patterns

--- django_project/project_name/urls.py
+++ django_project/project_name/urls.py
@@ -1,10 +1,10 @@
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.sitemaps import GenericSitemap
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-#from feincms.module.page.models import Page
-#from feincms.module.page.sitemap import PageSitemap
+# from feincms.module.page.models import Page
+# from feincms.module.page.sitemap import PageSitemap
 import os
 from django.views.generic import TemplateView, ListView
 
@@ -20,34 +20,35 @@ admin.autodiscover()
 ## OR
 # mysitemaps = {'pages' : PageSitemap}
 
-urlpatterns = patterns('',
+urlpatterns = [
         (r'', TemplateView.as_view(template_name="root.html")),
-)
+]
 
 # serve static content in debug mode
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$',
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$',
             'django.views.static.serve', {
             'document_root': settings.STATIC_ROOT,
             'show_indexes': True
         }),
-        (r'^(media/|static/)?medialibrary/(?P<path>.*)$',
+        url(r'^(media/|static/)?medialibrary/(?P<path>.*)$',
             'django.views.static.serve', {
             'document_root': '%s/medialibrary/' % settings.MEDIA_ROOT,
             'show_indexes': True
         }),
-        (r'^(?P<path>favicon.*)$', 'django.views.static.serve', {
+        url(r'^(?P<path>favicon.*)$', 'django.views.static.serve', {
             'document_root': settings.MEDIA_ROOT,
         }),
-        (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    )
+        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    ]
 
 
-urlpatterns += patterns('',
+urlpatterns += [
     # (r'^admin_tools/', include('admin_tools.urls')),
-    (r'^admin/', include(admin.site.urls)),    
-    # (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': mysitemaps}),
+    url(r'^admin/', admin.site.urls),    
+    # url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': mysitemaps}),
+    # FeinCMS 1.12- sitemaps don’t work with Django 1.10+
     # url(r'', include('feincms.urls')),
-)
+]
