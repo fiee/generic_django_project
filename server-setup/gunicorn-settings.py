ca15fa6670bd1436a5191e1e1f123ed049762643
Henning Hraban Ramm <hraban@fiee.net>
2017-05-09 22:37:58 +0200

    use dotenv for wsgi and gunicorn

--- server-setup/gunicorn-settings.py
+++ server-setup/gunicorn-settings.py
@@ -5,7 +5,13 @@ import dotenv
 
 PROJECT_NAME = 'project_name'
 rootrel = lambda p: os.path.normpath(os.path.join('/var/www', PROJECT_NAME, p))
-dotenv.load_dotenv(rootrel('.env'))
+
+try:
+    # django-dotenv-rw (Python 2.7)
+    dotenv.load_dotenv(rootrel('.env'))
+except AttributeError:
+    # django-dotenv (Python 3)
+    dotenv.read_dotenv(rootrel('.env'))
 
 bind = 'unix:%s' % rootrel('run/django.socket')
 #bind = '127.0.0.1:8'+str(os.getuid())[1:]
