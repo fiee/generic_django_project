8a280ddcf846bee169c70f77b2751178e9ee6fb2
hraban <hraban@fiee.net>
2019-12-28 23:25:13 +0100

    new dotenv handling in manage.py

--- django_project/manage.py
+++ django_project/manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
 import sys
@@ -6,11 +6,16 @@ import dotenv
 import logging
 logging.captureWarnings(True)  # dotenv uses warnings
 
-current_dir = os.path.dirname(__file__)
-# try to load .env file from current directory (I use that in development); 
-# if that fails (you’ll get a harmless warning), try the project’s base path.
-if not dotenv.read_dotenv(os.path.join(current_dir, '.env')):
-    dotenv.read_dotenv(os.path.abspath(os.path.join(current_dir, '../../..', '.env'))) # '/var/www/project_name/.env'
+current_dir = os.path.abspath(os.path.dirname(__file__))
+# try to load .env file from current directory or look upwards
+while current_dir and not os.path.isfile(os.path.join(current_dir, '.env')):
+    current_dir = "/".join(os.path.split(current_dir)[:-1])
+
+env = os.path.join(current_dir, '.env')
+if not os.path.isfile(env):
+    print("Configuration environment file (.env) not found!")
+    sys.exit(1)
+dotenv.read_dotenv(env)
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
